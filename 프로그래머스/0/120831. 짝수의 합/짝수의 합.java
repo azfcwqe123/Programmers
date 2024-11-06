46238985b59810fdc4c7842b7046381e973c1015
azfcwqe123 <121239031+azfcwqe123@users.noreply.github.com>
2024-11-06 14:38:01 +0900

    [level 0] Title: 짝수의 합, Time: 0.04 ms, Memory: 79.5 MB -BaekjoonHub

--- 프로그래머스/0/120831. 짝수의 합/짝수의 합.java
+++ 프로그래머스/0/120831. 짝수의 합/짝수의 합.java
@@ -3,8 +3,8 @@ class Solution {
        
         int sum = 0;
         
-        for(int i=1; i<=n; i++) {
-            if(i % 2 == 0) sum += i;        
+        for(int i=2; i<=n; i+=2) {
+            sum += i;        
         }
         
         return sum;
