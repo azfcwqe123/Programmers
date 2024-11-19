955bdfeb64fe637c4b72e8f427c63ee991ef8070
azfcwqe123 <121239031+azfcwqe123@users.noreply.github.com>
2024-11-19 14:36:46 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.04 ms, Memory: 72.7 MB -BaekjoonHub

--- 프로그래머스/0/120822. 문자열 뒤집기/문자열 뒤집기.java
+++ 프로그래머스/0/120822. 문자열 뒤집기/문자열 뒤집기.java
@@ -1,27 +1,7 @@
 class Solution {
     public String solution(String my_string) {
         
-        StringBuilder sb = new StringBuilder();
-        
-        char[] arr = my_string.toCharArray();
-        
-        int lt = 0;
-        int rt = my_string.length() - 1;
-        
-        while(lt < rt) {
-            char tmp = arr[lt];
-            arr[lt] = arr[rt];
-            arr[rt] = tmp;
-            
-            lt++;
-            rt--;
-        }
-        
-        for(char x : arr) {
-            sb.append(x);
-        }
-        
-        return sb.toString();
+        return new StringBuilder(my_string).reverse().toString();
         
     }
 }
