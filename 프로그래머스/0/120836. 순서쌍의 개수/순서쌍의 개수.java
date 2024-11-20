73af3a82e6e2555ad7aca6df4e95c4f450295ca3
azfcwqe123 <121239031+azfcwqe123@users.noreply.github.com>
2024-11-20 17:50:08 +0900

    [level 0] Title: 순서쌍의 개수, Time: 4.94 ms, Memory: 77 MB -BaekjoonHub

--- 프로그래머스/0/120836. 순서쌍의 개수/순서쌍의 개수.java
+++ 프로그래머스/0/120836. 순서쌍의 개수/순서쌍의 개수.java
@@ -1,25 +1,13 @@
 class Solution {
     public int solution(int n) {
         
-        int[] check = new int[n+1];
-        int sum = 1;
+        int ans = 0;
         
-        for(int i=2; i<=n; i++) {
-            
-            while(true) {
-                 
-                if(n % i != 0) break;
-                
-                n /= i;
-                
-                check[i]++;
-            }
-            
-            if(check[i] != 0) {
-                sum *= (check[i] + 1);
-            }            
+        for(int i=1; i<=n; i++) {
+            if(n % i == 0) ans++;
         }
-   
-        return sum;
+        
+        return ans;
+        
     }
 }
