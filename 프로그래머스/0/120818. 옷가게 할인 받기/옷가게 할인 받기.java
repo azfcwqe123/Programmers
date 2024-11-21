6e9be13bbbb5b6ba9d6c651ea21d0a787c3115de
azfcwqe123 <121239031+azfcwqe123@users.noreply.github.com>
2024-11-21 12:41:37 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.03 ms, Memory: 79.1 MB -BaekjoonHub

--- 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.java
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.java
@@ -1,13 +1,9 @@
 class Solution {
     public int solution(int price) {
         
-        if(100_000 <= price && price < 300_000) {
-            return (int) (price * 0.95);   
-        } else if(300_000 <= price && price < 500_000) {
-            return (int) (price * 0.9);
-        } else if(price >= 500_000) {
-            return (int) (price * 0.8);
-        }
+        if(price >= 500_000) return (int) (price * 0.8);
+        if(price >= 300_000) return (int) (price * 0.9);
+        if(price >= 100_000) return (int) (price * 0.95);
         
         return price;
     }
