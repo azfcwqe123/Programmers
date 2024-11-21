ac7f56c99b8deff2ec283d843c4e040cba8dd0e8
azfcwqe123 <121239031+azfcwqe123@users.noreply.github.com>
2024-11-21 13:17:45 +0900

    [level 0] Title: 개미 군단, Time: 0.02 ms, Memory: 74.2 MB -BaekjoonHub

--- 프로그래머스/0/120837. 개미 군단/개미 군단.java
+++ 프로그래머스/0/120837. 개미 군단/개미 군단.java
@@ -1,17 +1,7 @@
 class Solution {
     public int solution(int hp) {
-        
-        int ans = 0;
-        
-        ans += (hp / 5);
-        hp %= 5;
-        
-        ans += (hp / 3);
-        hp %= 3;
-        
-        ans += hp;
-        
-        return ans;
+                
+        return (hp / 5) + (hp % 5) / 3 + hp % 5 % 3;
         
     }
 }
