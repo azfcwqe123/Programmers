efa46af5a6dedb379784ee2f28302cdddd24243c
azfcwqe123 <121239031+azfcwqe123@users.noreply.github.com>
2024-11-24 13:38:34 +0900

    [level 0] Title: 숫자 찾기, Time: 0.04 ms, Memory: 71.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(int num, int k) {
+        
+        int ans = String.valueOf(num).indexOf(String.valueOf(k));
+        
+        return ans == -1 ? -1 : ++ans;
+        
+    }
+}
