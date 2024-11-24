da907d74408bc5a4388a7aa224ad667b610a956f
azfcwqe123 <121239031+azfcwqe123@users.noreply.github.com>
2024-11-24 22:56:44 +0900

    [level 2] Title: 올바른 괄호, Time: 27.66 ms, Memory: 53.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,32 @@
+import java.util.HashMap;
+import java.util.Stack;
+
+class Solution {
+    boolean solution(String s) {
+        
+        HashMap<Character, Character> map = new HashMap<>();
+        
+        map.put(')', '(');
+        map.put('}', '{');
+        map.put(']', '[');
+        
+        Stack<Character> stack = new Stack<>();
+        
+        for(char x : s.toCharArray()) {
+            
+            if(map.containsKey(x)) {
+                
+                if(stack.isEmpty()) return false;
+                else if(stack.pop().equals(map.get(x))) continue;
+            }
+            
+            else {
+                stack.push(x);
+            }
+            
+        }
+        
+        return stack.isEmpty();
+        
+    }
+}
