30464eb9290bc2ba967ad18f3e89ffddd57d96f2
azfcwqe123 <121239031+azfcwqe123@users.noreply.github.com>
2024-11-25 12:36:03 +0900

    [level 0] Title: 모스부호 (1), Time: 0.21 ms, Memory: 76.2 MB -BaekjoonHub

--- 프로그래머스/0/120838. 모스부호 （1）/모스부호 （1）.java
+++ 프로그래머스/0/120838. 모스부호 （1）/모스부호 （1）.java
@@ -1,50 +1,23 @@
-import java.util.HashMap;
-
 class Solution {
     public String solution(String letter) {
         
+        String[] arr = {".-", "-...", "-.-.", "-..", ".", "..-.",
+                       "--.", "....", "..", ".---", "-.-", ".-..", 
+                       "--", "-.", "---", ".--.", "--.-", ".-.",
+                       "...", "-", "..-", "...-", ".--", "-..-",
+                       "-.--", "--.."};
+        
+        String[] mos = letter.split(" ");
+        
         StringBuilder sb = new StringBuilder();
         
-        HashMap<String, Character> map = new HashMap<>() {
-            {
-            put(".-", 'a');
-            put("-...", 'b');
-            put("-.-.", 'c');
-            put("-..", 'd');
-            put(".", 'e');
-            put("..-.", 'f');
-            put("--.", 'g');
-            put("....", 'h');
-            put("..", 'i');
-            put(".---", 'j');
-            put("-.-", 'k');
-            put(".-..", 'l');
-            put("--", 'm');
-            put("-.", 'n');
-            put("---", 'o');
-            put(".--.", 'p');
-            put("--.-", 'q');
-            put(".-.", 'r');
-            put("...", 's');
-            put("-", 't');
-            put("..-", 'u');
-            put("...-", 'v');
-            put(".--", 'w');
-            put("-..-", 'x');
-            put("-.--", 'y');
-            put("--..", 'z');
-            }
-            
-        };
-        
-            
-        String[] arr = letter.split(" ");
-        
-        for(String x : arr) {
-            
-            sb.append(map.get(x));
+        for(int i=0; i<mos.length; i++) {
+            for(int j=0; j<arr.length; j++) {
+                if(mos[i].equals(arr[j])) sb.append((char) (j + 'a'));
+            }    
         }
         
         return sb.toString();
+        
     }
 }
