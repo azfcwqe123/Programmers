ea3202e72f59696c39dcb4fd8e5b6d004cb98532
azfcwqe123 <121239031+azfcwqe123@users.noreply.github.com>
2024-11-25 11:44:35 +0900

    [level 0] Title: k의 개수, Time: 71.60 ms, Memory: 102 MB -BaekjoonHub

--- 프로그래머스/0/120887. k의 개수/k의 개수.java
+++ 프로그래머스/0/120887. k의 개수/k의 개수.java
@@ -1,18 +1,12 @@
 class Solution {
     public int solution(int i, int j, int k) {
         
-        int cnt = 0;
-        
+        StringBuilder sb = new StringBuilder();
         
         for(int a=i; a<=j; a++) {
-            
-            for(char x : String.valueOf(a).toCharArray()) {
-                
-                if(x - '0' == k) cnt++;
-                    
-            }
+            sb.append(a);    
         }
         
-        return cnt;
+        return String.valueOf(sb).replaceAll("[^" + k  +"]", "").length();
     }
 }
