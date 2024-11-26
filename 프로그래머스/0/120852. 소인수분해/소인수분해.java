d4e06fcae8e56f7d99ab544b4cf81df1ccb0463f
azfcwqe123 <121239031+azfcwqe123@users.noreply.github.com>
2024-11-26 13:31:28 +0900

    [level 0] Title: 소인수분해, Time: 3.59 ms, Memory: 79.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120852. 소인수분해/소인수분해.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+
+class Solution {
+    public int[] solution(int n) {
+            
+        ArrayList<Integer> list = new ArrayList<>();
+        
+        for(int i=2; i<=n; i++) {
+            
+            if(n % i == 0) {
+                
+                list.add(i);
+                
+                while(n % i == 0) {
+                    n /= i;   
+                }         
+                
+            }
+        }
+        
+        return list.stream().mapToInt(i->i).toArray();
+        
+    }
+}
