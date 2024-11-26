121998031f389090eef4470159b33f3da0ee4006
azfcwqe123 <121239031+azfcwqe123@users.noreply.github.com>
2024-11-26 16:23:35 +0900

    [level 0] Title: 문자열 계산하기, Time: 0.28 ms, Memory: 82.1 MB -BaekjoonHub

--- 프로그래머스/0/120902. 문자열 계산하기/문자열 계산하기.java
+++ 프로그래머스/0/120902. 문자열 계산하기/문자열 계산하기.java
@@ -1,23 +1,30 @@
+import java.util.Stack;
+
 class Solution {
     public int solution(String my_string) {
         
         String[] s = my_string.split(" ");
         
-        int sum = Integer.parseInt(s[0]);
+        Stack<String> stack = new Stack<>();
         
+        int sum = 0;
+        
+        stack.push(s[0]);
+            
         for(int i=1; i<s.length; i+=2) {
             
-            if(s[i].equals("+")) {
-                sum += Integer.parseInt(s[i+1]);
-            } 
+            if(s[i].equals("+")) {                
+                sum = Integer.parseInt(stack.pop()) + Integer.parseInt(s[i+1]);
+                stack.push(String.valueOf(sum));
+            }
             
             else if(s[i].equals("-")) {
-                sum -= Integer.parseInt(s[i+1]);
-            }       
-            
+                sum = Integer.parseInt(stack.pop()) - Integer.parseInt(s[i+1]);
+                stack.push(String.valueOf(sum));
+            }            
         }
         
-        return sum;
+        return Integer.parseInt(stack.pop());
         
     }
 }
