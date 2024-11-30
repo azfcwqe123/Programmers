20d63508f6faf0952fb0fc179806c8aa2f0e5b4b
azfcwqe123 <121239031+azfcwqe123@users.noreply.github.com>
2024-11-30 19:31:26 +0900

    [level 0] Title: OX퀴즈, Time: 0.19 ms, Memory: 84.3 MB -BaekjoonHub

--- 프로그래머스/0/120907. OX퀴즈/OX퀴즈.java
+++ 프로그래머스/0/120907. OX퀴즈/OX퀴즈.java
@@ -7,16 +7,13 @@ class Solution {
             
             String[] tmp = quiz[i].split(" ");
             
-            int res1 = 0, res2 = 0;
+            int res1 = 0, res2 = Integer.parseInt(tmp[4]);
             
             if(tmp[1].equals("+")) {
                 res1 += Integer.parseInt(tmp[0]) + Integer.parseInt(tmp[2]);
             } else {
                 res1 += Integer.parseInt(tmp[0]) - Integer.parseInt(tmp[2]);
-            }
-            
-            if(tmp[4].equals("-")) res2 = Integer.parseInt(tmp[4]);
-            else res2 = Integer.parseInt(tmp[4]);
+            }            
             
             if(res1 == res2) OX[i] = "O";
             else OX[i] = "X";
