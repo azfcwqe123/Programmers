d372b17d9a54b6152f8bb6b4fe417f627b42f807
azfcwqe123 <121239031+azfcwqe123@users.noreply.github.com>
2024-12-01 13:38:02 +0900

    [level 0] Title: 옹알이 (1), Time: 4.95 ms, Memory: 85.9 MB -BaekjoonHub

--- 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.java
+++ 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.java
@@ -6,9 +6,7 @@ class Solution {
         for(String x : babbling) {
             
             x = x.replaceAll("(aya|ye|woo|ma)", "");
-            
-            x = x.replaceAll("[0-9]", "");
-            
+                      
             if(x.isEmpty()) cnt++;
             
         }
